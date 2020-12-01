206773ab3becc14b37be51f2fd17de30de41e47f
John Bartucz <jbartucz@MacBook-Pro.rochmnaa.metronetinc.net>
2020-12-01 11:02:43 -0600

    solved part 1 and part 2

---
+++ day01.py
@@ -0,0 +1,44 @@
+
+
+def find2020():
+
+    nums = []
+
+    with open("day0101.txt", "r") as f:
+        for line in f:
+            nums.append(int(line))
+
+    nums.sort()
+    print(nums)
+
+    # part 1:
+    for i in nums:
+        for j in nums:
+            if i + j == 2020:
+                print(i*j)
+                # return
+
+    # part 2:
+    for i in nums:
+        for j in nums:
+            for k in nums:
+                if i + j + k == 2020:
+                    print(i*j*k)
+                    return
+
+    if True == False:
+        i = 0
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] + nums[j] == 2020:
+                print(nums[i]*nums[j])
+                return
+            if i + j < 2020:
+                i += 1
+            else:
+                j -= 1
+    
+    print("No answer")
+
+find2020()
