86c6298648c36178f728072be71fe3a390ea27ad
Mr. Bartucz <jobartucz@isd535.org>
2020-12-02 09:15:23 -0600

    # using 1 index :(

--- day0202.py
+++ day0202.py
@@ -6,8 +6,8 @@ with open("day0201.txt", "r") as f:
         (nums, letter, pw) = line.split()
         mn,mx = nums.split('-')
         letter = letter[:-1]
-        mn = int(mn) - 1
-        mx = int(mx) - 1
+        mn = int(mn) - 1 # using 1 index :(
+        mx = int(mx) - 1 # using 1 index :(
         if mx < len(pw):
             if pw[mn] == letter and pw[mx] != letter:
                 numvalid += 1
