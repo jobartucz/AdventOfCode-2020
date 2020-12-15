51ea715db987db522af3f79eb818c3cdf767fec0
John Bartucz <jobartucz@isd535.org>
2020-12-15 16:25:53 -0600

    brute force

--- day1302.py
+++ day1302.py
@@ -8,23 +8,22 @@ for i, b in enumerate(buses):
     if b != 'x':
         times[int(b)] = i
 
-print(times)
+# print(times)
 
 found = False
 starttime = 0
-adder = 0
-for time, delay in times.items():
-    adder += time
-    while starttime + :
+while found == False:
 
     found = True
     # print(starttime)
+    for time, delay in times.items():
         if (starttime + delay) % time != 0:
             found = False
             starttime += 17
             break
 
-if found == True:
-    print(starttime)
-else:
-    print("how did I get here?")
+with open("day13solution.txt", "w") as f:
+    if found == True:
+        f.write(str(starttime))
+    else:
+        f.write("how did I get here?")
