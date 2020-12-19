c7ced757def0981c602e7a384adbd41acdcc5403
John Bartucz <jobartucz@isd535.org>
2020-12-18 21:39:12 -0600

    solved part 1

--- day1801.py
+++ day1801.py
@@ -9,15 +9,68 @@ class Node:
     def __str__(self):
         return self.val
 
-def maketree(line):
-    return line
+digits = [str(x) for x in range(10)]
+
+def evaluate(phrase):
+
+    left = 0
+    operator = ''
+    right = 0
+    result = 0
+
+    todo = []
+    space1 = phrase.find(' ')
+    if space1 == -1:
+        return int(phrase)
+
+    i = 0
+    while i < len(phrase):
+        if phrase[i] == '(':
+            parens = 1
+            newphrase = ""
+            for c in phrase[i+1:]:
+                if c == '(':
+                    parens += 1
+                elif c == ')':
+                    parens -= 1
+                    if parens == 0:
+                        break
+                newphrase += c
+            todo.append(evaluate(newphrase))
+            i += len(newphrase) + 1
+        elif phrase[i] in ['+','-','*','/']:
+            todo.append(phrase[i])
+        elif phrase[i]  == ' ':
+            i += 1
+            continue
+        else: # it must be an integer
+            space1 = phrase[i:].find(' ')
+            if space1 == -1:
+                todo.append(int(phrase[i:]))
+            else:
+                todo.append(int(phrase[i:i+space1]))
+        i += 1
+
+    result = todo[0]
+    for i in range(1,len(todo)):
+        if todo[i] == '+':
+            result += todo[i+1]
+        elif todo[i] == '*':
+            result *= todo[i+1]
+        if todo[i] == '/':
+            result /= todo[i+1]
+        if todo[i] == '-':
+            result -= todo[i+1]
+
+    return result
 
 
 def evaluate_tree(tree):
     return 0
 
-with open("day18test.txt", "r") as f:
+with open("day18.txt", "r") as f:
+    sum = 0
     for line in f:
-        tree = maketree(line)
+        sum += evaluate(line.strip())
 
-evaluate_tree(tree)
+print(sum)
